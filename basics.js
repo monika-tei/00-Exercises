56c130ced5674954d900b6a383bd4155c184284a
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-03 10:42:07 +0100

    number exercises

--- basics.js
+++ basics.js
@@ -1,1 +1,16 @@
-console.log("hello test");
+"use strict";
+
+let str = 17;
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+const value = "2";
+if (value) {
+  console.log("value is truthy");
+} else {
+  console.log("value is falsy");
+}
