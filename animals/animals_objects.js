41fb3ea4f49b138e1da7d31b1cf1fd7208c1cb21
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-12 17:57:11 +0100

    found variables

--- animals/animals_objects.js
+++ animals/animals_objects.js
@@ -4,52 +4,66 @@ window.addEventListener("DOMContentLoaded", start);
 
 const allAnimals = [];
 
-function start( ) {
-    console.log("ready");
+//1. Create an object prototype - name it Animal.
+const animalPrototype = {
+  name: "",
+  desc: "",
+  type: "",
+  age: 0,
+};
 
-    loadJSON();
+function start() {
+  console.log("ready");
+
+  loadJSON();
 }
-
 
 function loadJSON() {
-    fetch("animals.json")
-    .then( response => response.json() )
-    .then( jsonData => {
-        // when loaded, prepare objects
-        prepareObjects( jsonData );
+  fetch("animals.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
     });
 }
 
-function prepareObjects( jsonData ) {
-    jsonData.forEach( jsonObject => {
-        // TODO: Create new object with cleaned data - and store that in the allAnimals array
-        
-        // TODO: MISSING CODE HERE !!!
-    });
+function prepareObjects(jsonData) {
+  jsonData.forEach((jsonObject) => {
+    // 2.0 Create a new object (named animal) from the Animal prototype ++
+    const animals = Object.create(animalPrototype);
 
-    displayList();
+    // Find the values for name, desc, and type from the jsonObject (it is recommended to create temporary variables for them)
+    let values = jsonObject.fullname.split(" ");
+
+    // Set the properties in the new object to those values
+    animals.name = values[0];
+    animals.desc = values[2];
+    animals.type = values[3];
+    animals.age = jsonObject.age;
+    // Add the new object to the array of animals
+  });
+
+  displayList();
 }
 
 function displayList() {
-    // clear the list
-    document.querySelector("#list tbody").innerHTML = "";
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
 
-    // build a new list
-    allAnimals.forEach( displayAnimal );
+  // build a new list
+  allAnimals.forEach(displayAnimal);
 }
 
-function displayAnimal( animal ) {
-    // create clone
-    const clone = document.querySelector("template#animal").content.cloneNode(true);
+function displayAnimal(animal) {
+  // create clone
+  const clone = document.querySelector("template#animal").content.cloneNode(true);
 
-    // set clone data
-    clone.querySelector("[data-field=name]").textContent = animal.name;
-    clone.querySelector("[data-field=desc]").textContent = animal.desc;
-    clone.querySelector("[data-field=type]").textContent = animal.type;
-    clone.querySelector("[data-field=age]").textContent = animal.age;
+  // set clone data
+  clone.querySelector("[data-field=name]").textContent = animal.name;
+  clone.querySelector("[data-field=desc]").textContent = animal.desc;
+  clone.querySelector("[data-field=type]").textContent = animal.type;
+  clone.querySelector("[data-field=age]").textContent = animal.age;
 
-    // append clone to list
-    document.querySelector("#list tbody").appendChild( clone );
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
 }
-
-
