08b5c7b341bcfe331f870b114e479bb180097606
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-12 16:57:02 +0100

    comparing objects

--- objects/objects.js
+++ objects/objects.js
@@ -49,3 +49,36 @@ console.log(person1.lastName); // returnds undefined
 console.log(person1); //viewing the whole object now, we see 3 values but no trace of lastName property that was just deleted.
 
 //DECLARING WITH CONST
+console.clear();
+const person2 = {
+  firstName: "Peter",
+  age: 44,
+  student: false,
+};
+
+console.log(person2);
+person2.age++;
+console.log(person2); // now the age: 45
+
+// variable itself cannot be reassigned
+const person3 = {
+  firstName: "Bob",
+  age: 54,
+  student: true,
+};
+
+//person2 = person3; //assignment error
+
+//MUTABILITY, REFERENCES, COPIES
+//same object can exist in multiple places
+console.clear();
+
+const human1 = {
+  firstName: "Peter",
+  age: 43,
+  student: false,
+};
+const human2 = human1; // human 2 is refering back to human1
+human2.firstName = "Bobby";
+console.log(human1.firstName); // human1 inherited the changes made in the copy human2!
+//when you modify person2 it also modified the property of human1
