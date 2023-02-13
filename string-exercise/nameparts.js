517c7d203b12aa58875ef93b8bb83006994affe1
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-13 17:32:41 +0100

    added small comment

--- string-exercise/nameparts.js
+++ string-exercise/nameparts.js
@@ -1,14 +1,13 @@
 "use strict";
 
-// Expect the name to be a const - you can’t modify it.
 const fullName = "Peter Heronimous Lind";
 
 const firstSpace = fullName.indexOf(" ");
 
 const lastSpace = fullName.lastIndexOf(" ");
 //split the string into three variables: firstName, middleName and lastName.
-// Hint: use indexOf and substring
 
+// I'm using the substring to extract a portion of a string (startIndex, endIndex)
 let firstName = fullName.substring(0, 5);
 console.log(firstName);
 
@@ -18,4 +17,4 @@ console.log(middleName);
 let lastName = fullName.substring(lastSpace + 1);
 console.log(lastName);
 
-/* Continued on.... */
+/* Continued.... */
