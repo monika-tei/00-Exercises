de640a932eb8469a9c38d7a4451d0fcdff225790
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-13 17:35:44 +0100

    start

---
+++ string-exercise/capitalization3.js
@@ -0,0 +1,9 @@
+"use strict";
+/**Given a single name string in an unknown case, e.g. “peter” or “PETER” - create a new string with the same name, where the third letter is uppercase, and the rest is lowercase. I.e. “peTer”. */
+
+//Hint: use substring, toUpperCase, toLowerCase and simple string concatenation
+
+const name = "Monika";
+
+// const version1 = "name."
+console.log(version1);
