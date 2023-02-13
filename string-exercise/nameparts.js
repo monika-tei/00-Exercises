fa74f2c53a0da0a753238c6620a760bd4a32b694
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-13 18:49:41 +0100

    continued....

--- string-exercise/nameparts.js
+++ string-exercise/nameparts.js
@@ -18,3 +18,12 @@ let lastName = fullName.substring(lastSpace + 1);
 console.log(lastName);
 
 /* Continued.... */
+
+/**Find the old code you made to split a string into three variables: firstName, middleName and lastName, and put it inside a function named getNameParts( fullname ), where fullname is the parameter, the string you want to split up.
+
+Use your original code with indexOf and substring, don't "cheat" and use .split()!
+
+Make sure that the function creates three new strings: firstName, middleName and lastName. Console.log them at the end of the function. You can console.log them as an object to make it easier, like console.log( { firstName, middleName, lastName } ); */
+
+function getNameParts(fullName) {}
+console.log("here we go again");
