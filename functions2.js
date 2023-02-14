620d3df86ad1a0fea3bba7b727452ebf2bb6ef39
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-14 16:47:07 +0100

    test

--- functions2.js
+++ functions2.js
@@ -8,12 +8,11 @@
 // console.log(result);
 
 //ex1
-
-function greeting(firstName) {
-  return `Hello ${firstName}`;
-}
-let result = greeting("Peter");
-console.log(result);
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// let result = greeting("Peter");
+// console.log(result);
 
 //ex2
 // function greeting(firstName) {
@@ -23,10 +22,15 @@ console.log(result);
 // console.log(result);
 
 //2
-console.log(greeting("any name")); // hello any name
+// console.log(greeting("any name")); // hello any name
 //3
-console.log(greeting()); // hello undefined
+// console.log(greeting()); // hello undefined
 //4
-const txt = `Greeting is ${greeting("a name")}`;
-console.log(txt); // Greeting is Hello a name;
+// const txt = `Greeting is ${greeting("a name")}`;
+// console.log(txt); // Greeting is Hello a name;
 //5
+
+//Just calling the function results in the returned value to be "thrown away"
+
+let firstName = fullName.substring(0, fullName.indexOf(" "));
+firstName = capitalize(firstName);
