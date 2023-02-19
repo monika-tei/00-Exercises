1ce8ed9205ecb60a542260635de5b5be382d147a
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-19 13:08:17 +0100

    init

---
+++ animals/animalbase-stars&winners/animal-filter.js
@@ -0,0 +1,185 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+let allAnimals = [];
+
+const buttons = document.querySelectorAll("button");
+
+const Animal = {
+  name: "",
+  desc: "-unknown animal-",
+  type: "",
+  age: 0,
+};
+
+const settings = {
+  filter: "all",
+  sortBy: "name",
+  sortDir: "asc",
+};
+
+function start() {
+  console.log("ready");
+  // TODO: Add event-listeners to btn
+  findButtons();
+  loadJSON();
+}
+
+//UI for the filter buttons
+function findButtons() {
+  document.querySelectorAll("[data-action='filter']").forEach((button) => button.addEventListener("click", chooseFilter));
+
+  document.querySelectorAll("[data-action='sort']").forEach((button) => button.addEventListener("click", chooseSort));
+}
+
+async function loadJSON() {
+  const response = await fetch("animals.json");
+  const jsonData = await response.json();
+
+  // when loaded, prepare data objects
+  prepareObjects(jsonData);
+}
+
+function prepareObjects(jsonData) {
+  allAnimals = jsonData.map(preapareObject);
+
+  // TODO: This might not be the function we want to call first
+  displayList(allAnimals);
+}
+
+function preapareObject(jsonObject) {
+  const animal = Object.create(Animal);
+
+  const texts = jsonObject.fullname.split(" ");
+  animal.name = texts[0];
+  animal.desc = texts[2];
+  animal.type = texts[3];
+  animal.age = jsonObject.age;
+
+  return animal;
+}
+
+function chooseFilter(event) {
+  const filter = event.target.dataset.filter;
+  // event.target refers to the element that triggered the event, which in this case would be the button that was clicked.
+  //The dataset property is a way to access the data- attributes of an element in JavaScript.
+  // The above line is extracting the value of the data-filter attribute of the button element that was clicked and storing it in the filter variable. The value of filter will be either "cat" or "dog", depending on which button was clicked.
+  console.log(`user chose ${filter}`);
+  // filteredList(filter);
+  setFilter(filter);
+}
+
+function setFilter(filter) {
+  settings.filterBy = filter;
+
+  buildList();
+}
+// we want to create a filtered list before we display it
+// filter for cats, for dogs, and all animals
+
+function filterList(filteredList) {
+  // let filteredList = allAnimals;
+  if (settings.filterBy === "cat") {
+    //create a filtered list of only CATS
+    filteredList = allAnimals.filter(isCat);
+  } else if (settings.filterBy === "dog") {
+    filteredList = allAnimals.filter(isDog);
+  }
+
+  return filteredList;
+}
+function isCat(animal) {
+  console.log("found some cats");
+  return animal.type === "cat";
+}
+
+function isDog(animal) {
+  console.log("found some dogs");
+  return animal.type === "dog";
+}
+
+/** Moving onto the sorting part, begin the same as with filtering */
+
+function chooseSort(event) {
+  const sortBy = event.target.dataset.sort;
+  const sortDir = event.target.dataset.sortDirection;
+  //find old element and remove .sortBy
+
+  const oldElement = document.querySelector(`[data-sort="${settings.sortBy}"]`);
+  oldElement.classList.remove("sortBy");
+
+  // indicate active sort
+  event.target.classList.add("sortBy");
+  // toggles direction
+  if (sortDir === "asc") {
+    event.target.dataset.sortDirection = "desc";
+  } else {
+    event.target.dataset.sortDirection = "asc";
+  }
+
+  console.log(`user sorting by ${sortBy} and ${sortDir}`);
+  // sortList(sortBy, sortDir);
+  setSort(sortBy, sortDir);
+}
+
+function setSort(sortBy, sortDir) {
+  settings.sortBy = sortBy;
+  settings.sortDir = sortDir;
+
+  buildList();
+}
+
+function sortList(sortedList) {
+  // let sortedList = allAnimals;
+  let direction = 1;
+
+  if (settings.sortDir === "desc") {
+    direction = -1;
+  } else {
+    direction = 1;
+  }
+
+  sortedList = sortedList.sort(sortByProperty);
+
+  //this function needs to stay within the function;
+  function sortByProperty(animalA, animalB) {
+    if (animalA[settings.sortBy] < animalB[settings.sortBy]) {
+      return -1 * direction;
+    } else {
+      return 1 * direction;
+    }
+  }
+
+  return sortedList;
+}
+
+/** Next step... */
+function buildList() {
+  const currentList = filterList(allAnimals);
+  const sortedList = sortList(currentList);
+
+  displayList(sortedList);
+}
+
+function displayList(animals) {
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
+
+  // build a new list
+  animals.forEach(displayAnimal);
+}
+
+function displayAnimal(animal) {
+  // create clone
+  const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+  // set clone data
+  clone.querySelector("[data-field=name]").textContent = animal.name;
+  clone.querySelector("[data-field=desc]").textContent = animal.desc;
+  clone.querySelector("[data-field=type]").textContent = animal.type;
+  clone.querySelector("[data-field=age]").textContent = animal.age;
+
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
+}
